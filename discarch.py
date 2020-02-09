8144ffadf85e543513d968327799baf6b30186d8
josiah <josiah@slowling.local>
2020-02-09 02:44:28 +0000

    Remove /awspath, fix help text for / route, only return single value

--- discarch.py
+++ discarch.py
@@ -7,15 +7,9 @@ import pdb
 
 app = Flask(__name__)
 
-
-@app.route('/awspath')
-def lb_check():
-    return 'this is the index route'
-
-
 @app.route('/')
 def index():
-    return 'this is garbage. you probably meant to hit a different url.'
+    return "success! the server is up."
 
 
 @app.before_request
@@ -26,36 +20,7 @@ def log_request_info():
 
 @app.route('/discarch/mention', methods=['post'])
 def notify_slack_route():
-    post_json = request.form.to_dict(flat=False)
-    # this houses the json dict
-    convert_to_json = json.loads(post_json['payload'][0])
-    # this is the callback_id needed for later API calls
-    callback_id = convert_to_json.get('callback_id')
-    # this is the original message which can be modified for replacement
-    original_message = convert_to_json.get('original_message')
-    slack_attachments = [
-    {
-        "fallback": "buttons didn't work.",
-        "text": "BUTTONS.",
-        "attachment_type": "default",
-        "callback_id": 'non-unique',
-        "actions": [
-            {
-            "type": "button",
-            "text": "View investigation",
-            "url": 'https://me.jowj.net',
-            "style": "primary"
-            },
-            {
-            "type": "section",
-            "text": "BUTTONS."
-            }
-        ]
-        }
-    ]
-    # actually close the specified investigation.
-    original_message['attachments'] = slack_attachments
-    response = convert_to_json.get('original_message')
+    response = request.form['challenge']
     return(response)
 
 
